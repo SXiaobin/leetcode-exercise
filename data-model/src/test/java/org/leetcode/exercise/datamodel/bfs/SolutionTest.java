32a62cdebd92ac9ba54dce305c4b49ee43a264e1
Su, Xiaobin <xsu@psi.de>
2019-06-18 17:48:37 +0200

    墙与门 添加递归版测试用例

--- data-model/src/test/java/org/leetcode/exercise/datamodel/bfs/SolutionTest.java
+++ data-model/src/test/java/org/leetcode/exercise/datamodel/bfs/SolutionTest.java
@@ -19,4 +19,16 @@ public class SolutionTest {
     solution.wallsAndGates(rooms);
     Assert.assertArrayEquals(rooms, expected);
   }
+
+  @Test
+  public void wallsAndGates_A() {
+    int[][] rooms = {{INT, -1, 0, INT},
+            {INT, INT, INT, -1},
+            {INT, -1, INT, -1},
+            {0, -1, INT, INT}};
+    int[][] expected = {{3, -1, 0, 1}, {2, 2, 1, -1}, {1, -1, 2, -1}, {0, -1, 3, 4}};
+    SolutionA solution = new SolutionA();
+    solution.wallsAndGates(rooms);
+    Assert.assertArrayEquals(rooms, expected);
+  }
 }
